44563ace9585d50117ebcd694f507bd8fbfbb8df
王亮 <wangliang15@shein.com>
2023-02-28 13:11:40 +0800

    新增 远程触发action方法 测试成功

---
+++ remoteTrigger.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+async function trigger() {
+  const { data } = await axios({
+    url: `https://api.github.com/repos/MrWang6w/free_proxy_ip_pool/dispatches`,
+    headers: {
+      Accept: 'application/vnd.github.everest-preview+json',
+      Authorization: `token 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`,
+    },
+    method: 'post',
+    data: { event_type: 'webhook-run' },
+  });
+  console.log('data:', data);
+}
+
+// trigger()
