65d1ae00131d9d1a144b6919c2c7bc0eea235482
王亮 <wangliang15@shein.com>
2023-02-28 17:26:55 +0800

    修改提交格式

--- remoteTrigger.js
+++ remoteTrigger.js
@@ -1,19 +1,74 @@
 const axios = require('axios');
+const { Octokit } = require('octokit');
+const _ = require('lodash');
+async function testRun() {
+  const octokit = new Octokit({
+    auth: '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',
+  });
+
+  const { data: logsData } = await octokit.request(
+    'GET /repos/{owner}/{repo}/actions/workflows',
+    {
+      owner: 'MrWang6w',
+      repo: 'free_proxy_ip_pool',
+    }
+  );
+
+  const webHookAction = _.find(
+    _.get(logsData, 'workflows', []),
+    (x) => x.name.indexOf('webhook') !== -1
+  );
+
+  const data = await octokit.request(
+    'POST /repos/{owner}/{repo}/dispatches',
+    {
+      owner: 'MrWang6w',
+      repo: 'free_proxy_ip_pool',
+      event_type: 'webhook-run',
+      client_payload:{
+        logLevel: 111,
+
+      }
+    }
+  );
+  // const data = await octokit.request(
+  //   'POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches',
+  //   {
+  //     owner: 'MrWang6w',
+  //     repo: 'free_proxy_ip_pool',
+  //     event_type: 'webhook-run',
+  //     // inputs: {
+  //     //   logLevel: 111,
+  //     // },
+  //     workflow_id: _.get(webHookAction, 'id'),
+  //     ref: 'master',
+  //     inputs: {
+  //       logLevel: '111',
+  //     },
+  //   }
+  // );
+
+  console.log('data:', data);
+}
+
+testRun();
 
 async function trigger() {
   const { data } = await axios({
     url: `https://api.github.com/repos/MrWang6w/free_proxy_ip_pool/dispatches`,
     headers: {
-      Accept: 'application/vnd.github.everest-preview+json',
+      // Accept: 'application/vnd.github.everest-preview+json',
       Authorization: `token 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`,
     },
     method: 'post',
     data: {
-      event_type: 'webhook-run', 
-      inputs: {
-        logLevel: 111
-      }
+      event_type: 'webhook-run',
+      // inputs: {
+      //   logLevel: 111
+      // }
     },
+  }).catch((e) => {
+    console.log('e:', e);
   });
   console.log('data:', data);
 }
